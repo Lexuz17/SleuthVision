fcfa7c732147a0f9645589c41b1279a40693f41e
Jason Susanto <jasonsusanto17112003@gmail.com>
2024-04-26 11:58:30 +0700

    first Init

---
+++ SleuthVision/SleuthVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleuthVisionApp.swift
+//  SleuthVision
+//
+//  Created by Jason Susanto on 26/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SleuthVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
